4243ed58c7f0c74ee431514407c036bfe10976bd
정호진 <dnjfs@naver.com>
2024-08-04 00:59:37 +0900

    온라인 서브시스템으로 세션 생성
    - 호스팅 버튼 동작 변경
    GetOwningPlayer로 PlayerController를 받아와
    ISMLobbyController로 캐스팅 후 세션 생성 요청
    
    - 세션 생성
    CreateSession()으로 세션 생성 요청
    세션 생성 델리게이트 받으면 리슨 서버로 연 레벨로 이동

--- Source/ISeeMe/ISMLobbyController.cpp
+++ Source/ISeeMe/ISMLobbyController.cpp
@@ -4,7 +4,14 @@
 #include "ISMLobbyController.h"
 
 #include "Blueprint/UserWidget.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "Kismet/GameplayStatics.h"
 
+AISMLobbyController::AISMLobbyController()
+	: CreateSessionComplete(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete))
+{
+}
 
 void AISMLobbyController::BeginPlay()
 {
@@ -23,4 +30,65 @@ void AISMLobbyController::BeginPlay()
 	Mode.SetWidgetToFocus(UIWidgetInstance->GetCachedWidget());
 	SetInputMode(Mode);
 	SetShowMouseCursor(true);
+
+	if (GetSessionInterface() == false)
+	{
+		LOG_SCREEN("Failed GetSessionInterface()");
+		return;
+	}
+}
+
+bool AISMLobbyController::GetSessionInterface()
+{
+	IOnlineSubsystem* OSS = IOnlineSubsystem::Get();
+	if (OSS == nullptr)
+		return false;
+	
+	LOG_SCREEN("SubsystemName: %s", *OSS->GetSubsystemName().ToString());
+
+	OnlineSessionInterface = OSS->GetSessionInterface();
+	if (OnlineSessionInterface.IsValid() == false)
+		return false;
+
+	return true;
+}
+
+void AISMLobbyController::CreateSession()
+{
+	// NAME_GameSession 이름의 세션이 존재하는지 검사하여 파괴
+	if (FNamedOnlineSession* ExistingSession = OnlineSessionInterface->GetNamedSession(NAME_GameSession))
+	{
+		OnlineSessionInterface->DestroySession(NAME_GameSession);
+		LOG_SCREEN("Destroy session: %s", NAME_GameSession);
+	}
+
+	// 델리게이트 연결
+	OnlineSessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionComplete);
+
+	TSharedPtr<FOnlineSessionSettings> SessionSettings = MakeShareable(new FOnlineSessionSettings());
+	SessionSettings->NumPublicConnections = 2;		// 허용되는 플레이어 수
+	SessionSettings->bShouldAdvertise = true;		// 광고되는 세션인지 개인 세션인지
+	SessionSettings->bAllowJoinInProgress = true;	// 세션 진행중에 참여 허용
+	SessionSettings->bIsLANMatch = false;			// LAN에서만 실행되고 외부 공개되지 않음
+	SessionSettings->bIsDedicated = false;			// 데디케이티드 서버인지 (리슨 서버가 아닌지)
+	SessionSettings->bUsesPresence = true;			// Presence 사용 (유저 정보에 세션 정보를 표시하는듯)
+	SessionSettings->bAllowJoinViaPresence = true;	// Presence를 통해 참여 허용
+	SessionSettings->bUseLobbiesIfAvailable = true; // 플랫폼이 지원하는 경우 로비 API 사용
+
+	// 세션 생성
+	if (const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController())
+		OnlineSessionInterface->CreateSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *SessionSettings);
+}
+
+void AISMLobbyController::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	if (!bWasSuccessful)
+	{
+		LOG_SCREEN("Failed CreateSession()");
+		return;
+	}
+
+	LOG_SCREEN("Successful CreateSession()");
+	
+	UGameplayStatics::OpenLevel(this, TEXT("ThirdPersonMap"), true, "Listen");
 }
--- Source/ISeeMe/ISMLobbyController.h
+++ Source/ISeeMe/ISMLobbyController.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "ISMLobbyController.generated.h"
+
+#define	LOG_SCREEN(Format, ...) \
+	if (GEngine)\
+		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT(Format), ##__VA_ARGS__))
+
+DECLARE_DELEGATE_TwoParams(FOnCreateSessionCompleteDelegate, FName /*SessionName*/, bool /*bWasSuccessful*/);
 
 /**
  * 
@@ -14,12 +21,29 @@ class ISEEME_API AISMLobbyController : public APlayerController
 {
 	GENERATED_BODY()
 
+	AISMLobbyController();
+
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadwrite, Category = UI)
 	TSubclassOf<class UUserWidget> UIWidgetClass;
 
+private:
 	UPROPERTY()
 	TObjectPtr<class UUserWidget> UIWidgetInstance;
+
+// Online Subsystem
+protected:
+	bool GetSessionInterface();
+
+	UFUNCTION(BlueprintCallable)
+	void CreateSession();
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+private:
+	IOnlineSessionPtr OnlineSessionInterface;
+
+	FOnCreateSessionCompleteDelegate CreateSessionComplete;
 };
