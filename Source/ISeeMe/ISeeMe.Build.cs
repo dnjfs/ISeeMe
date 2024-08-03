f10da3b9b24b364e9d8b2d985fde36c547ec2aba
정호진 <dnjfs@naver.com>
2024-08-04 00:55:30 +0900

    온라인 서브시스템 적용
    Engine.ini 파일 설정
    OSS 모듈 추가
    OSS Steam 플러그인 추가

--- Source/ISeeMe/ISeeMe.Build.cs
+++ Source/ISeeMe/ISeeMe.Build.cs
@@ -8,6 +8,8 @@ public class ISeeMe : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam"
+        });
 	}
 }
