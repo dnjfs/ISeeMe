6c1c742e994866a6585022cf2c0dadb494d58cdb
정호진 <dnjfs@naver.com>
2024-09-13 00:13:32 +0900

    깜빡이는 바닥 구현
    - FlickerGround 클래스 추가
    서버에서만 틱 돌리기
    클라이언트 동기화를 위해 Multicast RPC 함수 호출
    맵에 배치된 액터에서만 깜빡이는 주기, 재우는 시간 세팅 가능
    머티리얼 인스턴스 다이나믹으로 Opacity 동적으로 변경하여 투명도 조절

---
+++ Source/ISeeMe/ISMFlickerGround.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ISMFlickerGround.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
+// Sets default values
+AISMFlickerGround::AISMFlickerGround()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GroundMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ground"));
+
+	RootComponent = GroundMesh;
+}
+
+// Called when the game starts or when spawned
+void AISMFlickerGround::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!HasAuthority())
+		SetActorTickEnabled(false);
+
+	// 액터의 스태틱 메시 컴포넌트를 가져옵니다.
+	UStaticMeshComponent* Mesh = FindComponentByClass<UStaticMeshComponent>();
+	if (Mesh && BaseMaterial)
+	{
+		// 머티리얼 인스턴스 다이나믹 생성
+		DynamicMaterial = UMaterialInstanceDynamic::Create(BaseMaterial, this);
+
+		// 메시에 머티리얼 인스턴스 적용
+		Mesh->SetMaterial(0, DynamicMaterial);
+	}
+
+	ResetTimer();
+}
+
+void AISMFlickerGround::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (!HasAuthority())
+		return;
+
+	if (bIsDormancy)
+		return;
+
+	RemainTime -= DeltaSeconds;
+
+	// 남은시간 30% 남을 때부터 투명도 세팅
+	if (RemainTime/FlickerInterval < 0.3f)
+		MulticastSetTransparency((RemainTime / FlickerInterval) / 0.3f);
+
+	if (RemainTime <= 0.f)
+	{
+		MulticastAwake(false);
+
+		FTimerHandle TimerHandle;
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this]() {
+			this->ResetTimer();
+		}, DormantTime, false);
+	}
+}
+
+void AISMFlickerGround::ResetTimer()
+{
+	RemainTime = FlickerInterval;
+
+	MulticastAwake(true);
+	MulticastSetTransparency(RemainTime);
+}
+
+void AISMFlickerGround::MulticastAwake_Implementation(bool bInAwake)
+{
+	if (GroundMesh)
+	{
+		GroundMesh->SetVisibility(bInAwake);
+		GroundMesh->SetCollisionEnabled(bInAwake ? ECollisionEnabled::QueryOnly : ECollisionEnabled::NoCollision);
+	}
+
+	bIsDormancy = !bInAwake;
+
+	if (bInAwake)
+		MulticastSetTransparency(1.f);
+}
+
+void AISMFlickerGround::MulticastSetTransparency_Implementation(float InOpacity)
+{
+	InOpacity = FMath::Clamp(InOpacity, 0.f, 1.f);
+
+	// 머티리얼 인스턴스의 "Opacity"라는 이름의 파라미터를 설정
+	if (DynamicMaterial)
+		DynamicMaterial->SetScalarParameterValue(TEXT("Opacity"), InOpacity);
+}
---
+++ Source/ISeeMe/ISMFlickerGround.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ISMFlickerGround.generated.h"
+
+UCLASS()
+class ISEEME_API AISMFlickerGround : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AISMFlickerGround();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	void ResetTimer();
+
+	UFUNCTION(NetMulticast, Unreliable)
+	void MulticastAwake(bool bInAwake);
+
+	UFUNCTION(NetMulticast, Unreliable)
+	void MulticastSetTransparency(float InOpacity);
+
+protected:
+
+private:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UMaterialInterface> BaseMaterial;
+	TObjectPtr<UMaterialInstanceDynamic> DynamicMaterial;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> GroundMesh;
+
+	bool bIsDormancy = false;
+
+	UPROPERTY(EditInstanceOnly, Category = GroundOption)
+	float FlickerInterval = 5.f;
+
+	UPROPERTY(EditInstanceOnly, Category = GroundOption)
+	float DormantTime = 3.f;
+
+	float RemainTime = 0.f;
+};
